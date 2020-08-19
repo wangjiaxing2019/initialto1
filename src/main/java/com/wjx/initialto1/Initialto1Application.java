73bc36e5e56af8a2fa340351059b2979194d2987
wangjiaxing <wangjiaxing2019@qq.com>
2020-08-19 17:34:10 +0800

    Initial commit

---
+++ src/main/java/com/wjx/initialto1/Initialto1Application.java
@@ -0,0 +1,13 @@
+package com.wjx.initialto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Initialto1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Initialto1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjx/initialto1/Initialto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjx.initialto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Initialto1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
